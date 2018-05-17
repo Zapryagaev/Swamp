eed6fa373f9cf560f6f8fc92cb76a4f9a56ac14d
Zapryagaev <spvolt@gmail.com>
2018-05-17 15:09:28 +0300

    extls

---
+++ pk_cut/pack2.pas
@@ -0,0 +1,23 @@
+unit rep_PipesSvodCountPoint;
+
+interface
+
+uses
+  SysUtils, Classes;
+
+type
+  Trep_PipesSvodCountPoint = class(TDataModule)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  rep_PipesSvodCountPoint: Trep_PipesSvodCountPoint;
+
+implementation
+
+{$R *.dfm}
+
+end.
